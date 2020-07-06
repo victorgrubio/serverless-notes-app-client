898f1f513edbf66755b7007e4ad5a00a7bba987d
Victor Garcia <victorgrubiodl@gmail.com>
2020-07-06 22:27:00 +0200

    Added AWS Amplify configuration

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "YOUR_API_GATEWAY_URL"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_yk7NhGgs7",
+    APP_CLIENT_ID: "676r7ge84s011ucr2jkhth33bf",
+    IDENTITY_POOL_ID: "99b14cbe-1e67-47b6-b89c-4b9ccf245706"
+  }
+};
